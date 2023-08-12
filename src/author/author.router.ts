a0532e31dfd3306b14697c6f0bae32a014ea8148
tienv1per <tienv1perds@gmail.com>
2023-08-12 18:06:40 +0700

    add api create author, validate req body

--- src/author/author.router.ts
+++ src/author/author.router.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import type { Request, Response } from "express";
 import {body, validationResult} from "express-validator";
-import { listAuthor } from "./author.service";
+import { listAuthor, getAuthor, createAuthor } from "./author.service";
 
 const router = express.Router();
 
@@ -13,6 +13,42 @@ router.get("/", async(req: Request, res: Response) => {
     } catch (error: any) {
         return res.status(500).json(error.message);
     }
+});
+
+// GET 1 unique author
+router.get("/:id", async(req: Request, res: Response) => {
+    const id: number = parseInt(req.params.id);
+    try {
+        const author = await getAuthor(id);
+        if(author){
+            return res.status(200).json(author);
+        }
+        return res.status(404).json("Author not found");
+        
+    } catch (error: any) {
+        return res.status(500).json(error.message);
+    }
+});
+
+// CREATE 1 author
+// params: 1st name, last name
+router.post("/", 
+    body("firstName").isString(),
+    body("lastName").isString(),
+    async(req: Request, res: Response) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(404).json({
+                errors: errors.array(),
+            });
+        }
+        try {
+            const author = req.body;
+            const newAuthor = await createAuthor(author);
+            return res.status(201).json(newAuthor);
+        } catch (error: any) {
+            return res.status(500).json(error.message);
+        }
 });
 
 export default router;
--- src/author/author.service.ts
+++ src/author/author.service.ts
@@ -23,7 +23,58 @@ export const getAuthor = async(id: number): Promise<Author|null> => {
     const author = await db.author.findUnique({
         where: {
             id: id,
+        },
+        select: {
+            id: true,
+            firstName: true,
+            lastName: true,
+            createdAt: true,
         }
     });
     return author;
+};
+
+// sử dụng Omit để loại bỏ thuộc tính "id" từ kiểu Author. Tức là tham số author được truyền vào hàm không nên có thuộc tính "id".
+export const createAuthor = async(author: Omit<Author, "id">): Promise<Author> => {
+    const {firstName, lastName} = author;
+    const createdAuthor = await db.author.create({
+        data: {
+            firstName: firstName,
+            lastName: lastName,
+        },
+        select: {
+            id: true,
+            firstName: true, 
+            lastName: true,
+            createdAt: true
+        }
+    });
+    return createdAuthor;
+};
+
+export const updateAuthor = async(author: Omit<Author, "id">, id: number): Promise<Author> => {
+    const {firstName, lastName} = author;
+    return db.author.update({
+        where: {
+            id: id,
+        },
+        data: {
+            firstName: firstName,
+            lastName: lastName,
+        },
+        select: {
+            id: true,
+            firstName: true, 
+            lastName: true,
+            createdAt: true
+        }
+    });
+};
+
+export const deleteAuthor = async(id: number) => {
+    await db.author.delete({
+        where: {
+            id: id,
+        }
+    });
 };
